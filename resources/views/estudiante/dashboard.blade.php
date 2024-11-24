44a40647fcbfe90155321fb441358b2c4e960af4
BryanGuapulema <bryan19012003@gmail.com>
2024-11-24 17:33:21 -0500

    Mejora UI creacion y llenado encuestas, dashboard estudiante, agradecimiento

--- resources/views/estudiante/dashboard.blade.php
+++ resources/views/estudiante/dashboard.blade.php
@@ -1,16 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+    <h1 class="text-2xl font-semibold text-gray-800">Bienvenido, ESTUDIANTE</h1>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome/>
-
-                <h1>Bienvenido, ESTUDIANTE</h1>
+                <x-welcome/>                
             </div>
         </div>
     </div>
