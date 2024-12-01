1b7e5888b9584883d4f8739be606269002c48080
BryanGuapulema <bryan19012003@gmail.com>
2024-11-30 21:36:12 -0500

    eliminacion survey_response y arreglo rutas

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserRoleController;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\DocenteController;
-use App\Http\Controllers\EstudianteController;
-use App\Http\Controllers\SurveyController;
-use App\Http\Controllers\ResponseController;
 use App\Http\Controllers\AsignaturaController;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\DocenteController;
 use App\Http\Controllers\EncuestaController;
-use App\Http\Controllers\PreguntaController;
-use App\Http\Controllers\OpcioneController;
-use App\Http\Controllers\PreguntaOpcionController;
 use App\Http\Controllers\EncuestaPreguntumController;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\OpcioneController;
+use App\Http\Controllers\PreguntaController;
+use App\Http\Controllers\PreguntaOpcionController;
 use App\Http\Controllers\RespuestaController;
 
 
@@ -23,7 +22,7 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum', 
     config('jetstream.auth_session'), 
-    'verified', // Aseguramos que el usuario esté verificado
+    'verified',
 ])->group(function () {
     Route::middleware('role:admin')->get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::middleware('role:docente')->get('/docente/dashboard', [DocenteController::class, 'dashboard'])->name('docente.dashboard');
@@ -35,7 +34,6 @@ Route::middleware([
 
 // Rutas solo accesibles por el rol 'admin'
 Route::middleware('role:admin')->group(function () {
-    //Route::get('/admin_dashboard', [AdminController::class, 'index'])->name('admin.index');
     Route::get('admin/users', [UserRoleController::class, 'index'])->name('admin.users.index');
     Route::post('admin/users/{user}/update-role', [UserRoleController::class, 'update'])->name('admin.users.update-role');
 });
@@ -43,35 +41,23 @@ Route::middleware('role:admin')->group(function () {
 
 // Rutas para los docentes
 Route::middleware(['auth', 'role:docente'])->group(function () {
-    Route::resource('surveys', SurveyController::class);
-    //Route::get('/docente/surveys/create', [SurveyController::class, 'create'])->name('surveys.create');
-    //Route::post('/docente/surveys', [SurveyController::class, 'store'])->name('surveys.store');
-    Route::get('/docente/surveys/{uuid}', [SurveyController::class, 'generated'])->name('surveys.generated');
-    //Route::get('/docente/surveys', [SurveyController::class, 'index'])->name('surveys.index');
-});
-
-Route::middleware(['auth'])->group(function () {
-    // Rutas para responder encuestas
-    Route::get('/survey/{uuid}', [ResponseController::class, 'show'])->name('survey.respond');
-    Route::post('/survey/{uuid}', [ResponseController::class, 'store']);
+    Route::get('/docente/encuestas/{id}', [EncuestaController::class, 'generated'])->name('encuestas.generated');
 });
 
 //CRUDS
-Route::resource('cursos', App\Http\Controllers\CursoController::class);
+Route::resource('cursos', CursoController::class);
 Route::resource('asignaturas', AsignaturaController::class);
-Route::resource('docentes', App\Http\Controllers\DocenteController::class);
+Route::resource('docentes', DocenteController::class);
 Route::resource('encuestas', EncuestaController::class);
-Route::get('/docente/encuestas/{id}', [EncuestaController::class, 'generated'])->name('encuestas.generated');
 
 Route::resource('preguntas', PreguntaController::class);
 Route::resource('opciones', OpcioneController::class);
 Route::resource('pregunta-opcions', PreguntaOpcionController::class);
 Route::resource('encuesta-pregunta', EncuestaPreguntumController::class);
 
-//RESPUESTAS
 Route::resource('respuestas', RespuestaController::class);
 
-
+//rutas para los estudiantes
 Route::middleware(['auth', 'role:estudiante'])->group(function () {
     // Muestra el formulario para responder la encuesta
     Route::get('/encuestas/{uuid}/responder', [EncuestaController::class, 'responder'])->name('encuestas.responder');
